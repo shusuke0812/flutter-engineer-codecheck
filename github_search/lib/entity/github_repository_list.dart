67a0fb56fc71920fad1807fc0f82de266063d1dd
shusuke0812 <shu.ota0812@gmail.com>
2023-09-07 01:24:37 +0900

    add: entity -> github_repository_list

---
+++ github_search/lib/entity/github_repository_list.dart
@@ -0,0 +1,17 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import 'github_repository.dart';
+
+part 'github_repository_list.freezed.dart';
+part 'github_repository_list.g.dart';
+
+@freezed
+class GitHubRepositoryList with _$GitHubRepositoryList {
+  const factory GitHubRepositoryList({
+    @JsonKey(name: 'total_count') required int totalCount,
+    @JsonKey(name: 'incomplete_results') bool? incompleteResults,
+    required List<GitHubRepository> items,
+  }) = _GitHubRepositoryList;
+
+  factory GitHubRepositoryList.fromJson(Map<String, dynamic> json) => _GitHubRepositoryListFromJson(json);
+}
