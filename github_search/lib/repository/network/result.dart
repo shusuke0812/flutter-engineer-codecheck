824305c4a9a3fdc3ace411945c6affe8fd1e579f
shusuke0812 <shu.ota0812@gmail.com>
2023-09-07 23:57:49 +0900

    add: comment

--- github_search/lib/repository/network/result.dart
+++ github_search/lib/repository/network/result.dart
@@ -2,6 +2,9 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'result.freezed.dart';
 
+// Reference
+// - https://codewithandrea.com/articles/flutter-exception-handling-try-catch-result-type/
+
 @freezed
 abstract class Result<T> with _$Result<T> {
   const factory Result.success(T value) = Success<T>;
