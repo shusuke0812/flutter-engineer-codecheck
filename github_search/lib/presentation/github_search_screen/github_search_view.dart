613d91eae501c976c07c25e5162a60b0d1a29e25
shusuke0812 <shu.ota0812@gmail.com>
2023-09-10 00:31:56 +0900

    add: search field in GitHubSearchView

--- github_search/lib/presentation/github_search_screen/github_search_view.dart
+++ github_search/lib/presentation/github_search_screen/github_search_view.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 import 'package:github_search/presentation/github_search_screen/github_search_viewmodel.dart';
 import 'package:github_search/presentation/github_search_screen/widget/github_repository_list_widget.dart';
+import 'package:github_search/presentation/github_search_screen/widget/github_search_text_field_widget.dart';
 import 'package:provider/provider.dart';
 
 class GitHubSearchView extends StatefulWidget {
@@ -17,8 +18,20 @@ class _GitHubSearchViewState extends State<GitHubSearchView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text(widget.title)),
-      body: GitHubRepositoryListWidget(
-        list: Provider.of<GitHubSearchViewModel>(context).list
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          GitHubSearchTextFieldWidget(
+            onSubmitted: (String searchWord) {
+              Provider.of<GitHubSearchViewModel>(context, listen: false).loadRepositoryList(searchWord);
+            },
+          ),
+          Expanded(
+            child: GitHubRepositoryListWidget(
+              list: Provider.of<GitHubSearchViewModel>(context).list
+            )
+          )
+        ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
