d72ec26f4793b63249891825f668ed62a366bccd
shusuke0812 <shu.ota0812@gmail.com>
2023-09-10 01:59:41 +0900

    change: github serch field style

--- github_search/lib/presentation/github_search_screen/widget/github_search_text_field_widget.dart
+++ github_search/lib/presentation/github_search_screen/widget/github_search_text_field_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class GitHubSearchTextFieldWidget extends StatelessWidget {
   const GitHubSearchTextFieldWidget({
@@ -12,11 +13,31 @@ class GitHubSearchTextFieldWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      style: const TextStyle(fontSize: 17, color: Colors.grey),
-      controller: TextEditingController(),
-      onSubmitted: onSubmitted,
-      onChanged: onChanged, // NOTE: for incremental search
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: TextField(
+        style: const TextStyle(fontSize: 17, color: Colors.blueGrey),
+        controller: TextEditingController(),
+        onSubmitted: onSubmitted,
+        onChanged: onChanged,
+        decoration: const InputDecoration(
+          border: InputBorder.none,
+          enabledBorder: OutlineInputBorder( // TODO: follow DRY
+            borderRadius: BorderRadius.all(Radius.circular(34)),
+            borderSide: BorderSide(color: Colors.blueGrey)
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(34)),
+            borderSide: BorderSide(color: Colors.blueGrey)
+          ),
+          prefixIcon: Icon(
+            FontAwesomeIcons.magnifyingGlass,
+            color: Colors.blueGrey,
+            size: 17,
+          ),
+          hintText: "Search GitHub"
+        ), // NOTE: for incremental search
+      ),
     );
   }
 }
