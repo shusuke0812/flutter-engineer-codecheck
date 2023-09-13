9de935b40eb9822724fd4097adc2b16ef1ba228a
shusuke0812 <shu.ota0812@gmail.com>
2023-09-13 22:31:16 +0900

    add: GitHubRepositoryDetalViewModel

--- github_search/lib/presentation/github_repository_detail_screen/github_repository_detail_viewmodel.dart
+++ github_search/lib/presentation/github_repository_detail_screen/github_repository_detail_viewmodel.dart
@@ -1,4 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:github_search/repository/github_readme_repository.dart';
+import 'package:github_search/utility/logger.dart';
 
 class GitHubRepositoryDetalViewModel with ChangeNotifier {
+  GitHubRepositoryDetalViewModel({required this.githubReadmeRepository});
+
+  final GitHubReadmeRepository githubReadmeRepository;
+
+  // Success notify values
+  String _htmlUrl = "";
+  String get htmlUrl {
+    return _htmlUrl;
+  }
+
+  // Error notifiy values
+  String _errorMessage = "";
+  String get errorMessage {
+    return _errorMessage;
+  }
+
+  Future<void> loadRepositoryReadme(String ownerName, String repositoryName) async {
+    final result = await githubReadmeRepository.getReadme(ownerName: ownerName, repositoryName: repositoryName);
+    result.when(
+      success: (data) {
+        logger.d("response=$data");
+        _htmlUrl = data.htmlUrl;
+        notifyListeners();
+      }, 
+      error: (error) {
+        _errorMessage = "Not found README";
+        notifyListeners();
+      }, 
+      exception: (exception) {
+        // do nothing
+      }
+    );
+  }
 }
