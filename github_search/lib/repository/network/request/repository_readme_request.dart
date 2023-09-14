33f44c379a96825e73d9b98730af919f6616848a
shusuke0812 <shu.ota0812@gmail.com>
2023-09-14 22:18:14 +0900

    fix: RepositoryReadmeRequest's parameter

--- github_search/lib/repository/network/request/repository_readme_request.dart
+++ github_search/lib/repository/network/request/repository_readme_request.dart
@@ -3,6 +3,9 @@ import 'package:github_search/repository/network/http_method.dart';
 import 'package:github_search/repository/network/request/request_interface.dart';
 
 
+// Reference
+// - doc: https://docs.github.com/ja/free-pro-team@latest/rest/repos/contents?apiVersion=2022-11-28#get-a-repository-readme
+
 class RepositoryReadmeRequest extends RequestInterface {
   RepositoryReadmeRequest({
     required this.repositoryName,
@@ -22,7 +25,7 @@ class RepositoryReadmeRequest extends RequestInterface {
   String get path { return "/repos/$ownerName/$repositoryName/readme"; }
 
   @override
-  Future<Map<String ,String>> parameters() async {
-    return {};
+  Future<Map<String ,String>?> parameters() async {
+    return null;
   }
 }
