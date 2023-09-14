e32f64384c67d07e056851010a87ed7cedc706f1
shusuke0812 <shu.ota0812@gmail.com>
2023-09-14 22:35:53 +0900

    add: ChangeNotifierProvider of GitHubRepositoryDetailViewModel

--- github_search/lib/main.dart
+++ github_search/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:github_search/presentation/github_repository_detail_screen/github_repository_detail_viewmodel.dart';
 import 'package:github_search/presentation/github_search_screen/github_search_viewmodel.dart';
+import 'package:github_search/repository/github_readme_repository.dart';
 import 'package:github_search/repository/github_search_repository.dart';
 import 'package:github_search/utility/router/router.dart';
 import 'package:provider/provider.dart';
@@ -15,7 +17,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-        ChangeNotifierProvider.value(value: GitHubSearchViewModel(githubSearchRepository: GitHubSearchRepository()))
+        ChangeNotifierProvider.value(value: GitHubSearchViewModel(githubSearchRepository: GitHubSearchRepository())),
+        ChangeNotifierProvider.value(value: GitHubRepositoryDetailViewModel(githubReadmeRepository: GitHubReadmeRepository()))
       ],
       child: MaterialApp.router(
         routerDelegate: appRouter.routerDelegate,
