0ce0d04ae82c08e31fd820c329bf63094421050b
shusuke0812 <shu.ota0812@gmail.com>
2023-09-17 12:15:12 +0900

    fix: GitHubRepositoryDetailViewModel DI of repository class

--- github_search/lib/main.dart
+++ github_search/lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:github_search/presentation/github_repository_detail_screen/github_repository_detail_viewmodel.dart';
 import 'package:github_search/presentation/github_search_screen/github_search_viewmodel.dart';
 import 'package:github_search/repository/github_readme_repository.dart';
+import 'package:github_search/repository/github_repo_repository.dart';
 import 'package:github_search/repository/github_search_repository.dart';
 import 'package:github_search/utility/router/router.dart';
 import 'package:provider/provider.dart';
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
     return MultiProvider(
       providers: [
         ChangeNotifierProvider.value(value: GitHubSearchViewModel(githubSearchRepository: GitHubSearchRepository())),
-        ChangeNotifierProvider.value(value: GitHubRepositoryDetailViewModel(githubReadmeRepository: GitHubReadmeRepository()))
+        ChangeNotifierProvider.value(value: GitHubRepositoryDetailViewModel(githubReadmeRepository: GitHubReadmeRepository(), gitHubRepoRepository: GitHubRepoRepository()))
       ],
       child: MaterialApp.router(
         routerDelegate: appRouter.routerDelegate,
