53a5a44027968c13aeef857c7f62d3774d2bd64d
shusuke0812 <shu.ota0812@gmail.com>
2024-02-18 13:58:58 +0900

    fix: status bar color

--- github_search/lib/main.dart
+++ github_search/lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:github_search/utility/router/router.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.blueGrey,
+  ));
+
   runApp(
     const ProviderScope(
       child: MyApp()
