962c7107d6bc1959b5647a119685c833213244bb
shusuke0812 <shu.ota0812@gmail.com>
2024-04-18 21:53:12 +0900

    add: sentry_flutter package

--- github_search/macos/Flutter/GeneratedPluginRegistrant.swift
+++ github_search/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import package_info_plus
+import sentry_flutter
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  SentryFlutterPlugin.register(with: registry.registrar(forPlugin: "SentryFlutterPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
