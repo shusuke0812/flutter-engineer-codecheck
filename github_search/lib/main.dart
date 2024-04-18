c53e4e0e4ccb8185fea82bfdd3a0bfd4d6078d1e
shusuke0812 <shu.ota0812@gmail.com>
2024-04-18 22:45:04 +0900

    fix: style

--- github_search/lib/main.dart
+++ github_search/lib/main.dart
@@ -11,7 +11,7 @@ void main() async {
     statusBarColor: Colors.blueGrey,
   ));
 
-  await dotenv.load(fileName: ".env");
+  await dotenv.load(fileName: '.env');
   await SentryFlutter.init(
     (options) {
       options.dsn = dotenv.get('SENTRY_DSN');
